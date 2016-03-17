1dfc215cebb552ad000036505f26fdf850dc364e
Nicholas Grynkevych <nicholasg@rambler.ru>
2016-03-17 22:38:52 +0200

    Added records for DB

--- src/main/resources/db-init.sql
+++ src/main/resources/db-init.sql
@@ -1,31 +1,31 @@
 INSERT INTO developer_info(name, country, city, street, zipcode, phone_number, website, email, fax)
-  VALUES('name', 'c', 'city', 'st', 'zip', '380665329987', 'website', 'email', 'fax');
--- INSERT INTO developer_info(name, country, city, street, zipcode, phone_number, website, email, fax)
---   VALUES();
--- INSERT INTO developer_info(name, country, city, street, zipcode, phone_number, website, email, fax)
---   VALUES();
--- INSERT INTO developer_info(name, country, city, street, zipcode, phone_number, website, email, fax)
---   VALUES();
--- INSERT INTO developer_info(name, country, city, street, zipcode, phone_number, website, email, fax)
---   VALUES();
+  VALUES('Михайло', 'Україна', 'Івано-Франківськ', 'Сагайдачного 26', '00073', '0965727366', 'mihalich.if.com', 'mishka.b@gmail.com', '034309');
+INSERT INTO developer_info(name, country, city, street, zipcode, phone_number, website, email, fax)
+  VALUES('Лёха', 'Россия', 'Петербург', 'Мартовская 8', '04777', '7089673456', 'whereislesha.ru', 'aleksaner.br@mail.ru', '77234');
+INSERT INTO developer_info(name, country, city, street, zipcode, phone_number, website, email, fax)
+  VALUES('Adobe inc.', 'USA', 'San-Francisco', 'flycacs 44a', '99976', '5550955', 'adobe.com', 'ucanseeme@gmail.com', '550005');
+INSERT INTO developer_info(name, country, city, street, zipcode, phone_number, website, email, fax)
+  VALUES('Microsoft', 'USA', 'Редмонд', 'gaben street 3', '55444', '555055', 'microsoft.com', 'supportmicrosoft@gmail.com', '550504');
+INSERT INTO developer_info(name, country, city, street, zipcode, phone_number, website, email, fax)
+  VALUES('Google', 'USA', 'Каліфорнія', 'Silicon Valley', '55232', '555333', 'google.com', 'supportgogle@gmail.com', '552002');
 
 INSERT INTO license_info(name, minimum_users, maximum_users, expiration, price_for_one, price_for_ten, price_for_hundred, type)
-  VALUES('namee', 1, 100, 255, 25.50, 30.25, 40.75, 2);
--- INSERT INTO license_info(name, minimum_users, maximum_users, expiration, price_for_one, price_for_ten, price_for_hundred, type)
---   VALUES();
--- INSERT INTO license_info(name, minimum_users, maximum_users, expiration, price_for_one, price_for_ten, price_for_hundred, type)
---   VALUES();
+  VALUES('license1', 1, 100, 256, 0, 0, 0, 0);
+INSERT INTO license_info(name, minimum_users, maximum_users, expiration, price_for_one, price_for_ten, price_for_hundred, type)
+  VALUES('license2', 1, 1000, 356, 499.99, 3999.99, 29999.99, 1);
+INSERT INTO license_info(name, minimum_users, maximum_users, expiration, price_for_one, price_for_ten, price_for_hundred, type)
+  VALUES('license3', 10, 2000, 450, 5000.0, 25000.0, 50000.0, 2);
 
 INSERT INTO software_info(name, version, release, windows, macos, linux, developer_id, license_id)
-  VALUES('nam', '14.01', '2015-04-04', TRUE, FALSE, FALSE, 1, 1);
+  VALUES('Office', '2013', '2012-01-12', true, true, false, 1, 3);
 INSERT INTO software_info(name, version, release, windows, macos, linux, developer_id, license_id)
-  VALUES('nam2', '16.02', '2016-03-02', TRUE, TRUE, TRUE, 1, 1);
--- INSERT INTO software_info(name, version, release, windows, macos, linux, developer_id, license_id)
---   VALUES();
--- INSERT INTO software_info(name, version, release, windows, macos, linux, developer_id, license_id)
---   VALUES();
--- INSERT INTO software_info(name, version, release, windows, macos, linux, developer_id, license_id)
---   VALUES();
+  VALUES('Adobe Photoshop', 'cc15', '2015-05-21', true, true, true, 2, 1);
+INSERT INTO software_info(name, version, release, windows, macos, linux, developer_id, license_id)
+  VALUES('Skype', '7.21.0.100', '2016-01-31', true, true, true, 3, 2);
+INSERT INTO software_info(name, version, release, windows, macos, linux, developer_id, license_id)
+  VALUES('Android', '4.4 KitKat', '2013-11-30', false, false, false, 4, 1);
+INSERT INTO software_info(name, version, release, windows, macos, linux, developer_id, license_id)
+  VALUES('UltraISO', '5.44', '2012-04-02', true, false, false, 5, 2);
 
 -- developer_info (city varchar(50), country varchar(50), email varchar(200), fax varchar(20), name varchar(200), phone_number varchar(20), street varchar(50), website varchar(200), zipcode varchar(50)
 -- license_info (expiration integer, maximum_users integer, minimum_users integer, name varchar(250), price_for_hundred double, price_for_one double, price_for_ten double, type integer)
